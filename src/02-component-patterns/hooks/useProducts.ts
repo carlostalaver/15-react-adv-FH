a0d14c24aadf9b20f303aa237936b3af162590fa
ctalavera <ctalavera@bch.bancodechile.cl>
2022-02-03 18:21:46 -0300

    patron control props alternatico implementado

--- src/02-component-patterns/hooks/useProducts.ts
+++ src/02-component-patterns/hooks/useProducts.ts
@@ -10,14 +10,8 @@ interface IUseProductArgs {
 export const useProduct = ( {onChangeCustom, product, value = 0}: IUseProductArgs ) => { // no se usó {} en ( onChangeCustom?: () => void ) xq este es un hook(funcion silvestre) personalizado, no un componente REACT
     const [counter, setCounter] = useState  ( value );
 
-    /* isControlled es para saber si el componente que hace uso de ese hooks es controlado con la funcion  onChangeCustom*/
-    const isControlled = useRef(!!onChangeCustom);
-
     const increaseBy =(value: number) => {
 
-        if(isControlled.current){
-            return onChangeCustom!({product, count: value});// Ojo notar el operador de asercion no nulo
-        }
         let newValue = Math.max( value + counter, 0);
 
         setCounter(newValue);
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -3,28 +3,22 @@ import { IProduct, IProductInCart, IShoppingCart } from "../interfaces/interface
 import { products } from "../data/products";
 
 export const useShoppingCart = ()  => {
-    
+
     const [shoppingCart, setShoppingCart] = useState<IShoppingCart>({});
 
     const onProductCounterChange = ({ count, product }: { count: number, product: IProduct }) => {
 
         setShoppingCart(oldShoppingCart => {
             
-            const  productInCart: IProductInCart  = oldShoppingCart[product.id] || {...product, count: 0};
-
-
-            if(Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return{
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                } 
+            if(count === 0){
+                const  { [product.id]: toDelete, ...rest } =  oldShoppingCart;
+                return rest;
             }
 
-            //cuando el count sea menor o igual a cero elimino el producto del state
-            const { [product.id]: toDelete, ...resto } = oldShoppingCart;
+
             return { 
-                ...resto
+                ...oldShoppingCart,
+                [product.id]: {...product, count}
             };
         })
 
